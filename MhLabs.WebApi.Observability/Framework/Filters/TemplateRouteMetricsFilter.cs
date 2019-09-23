c95a2e104ca61843c24d6d07e079b8613591ab74
Mattias Liljenzin <mattias.liljenzin@gmail.com>
2019-09-23 18:57:10 +0200

    feat: add template route metric filter

---
+++ MhLabs.WebApi.Observability/Framework/Filters/TemplateRouteMetricsFilter.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.AspNetCoreServer;
+using MhLabs.SerilogExtensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ecom_order_subscription.Framework.Filters
+{
+    public class TemplateRouteMetricsFilter : IAsyncResourceFilter
+    {
+        public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+        {
+            var templateRoute = context.ActionDescriptor?.AttributeRouteInfo?.Template?.ToLowerInvariant();
+            var proxyRequest = (APIGatewayProxyRequest) context.HttpContext.Items[AbstractAspNetCoreFunction.LAMBDA_REQUEST_OBJECT];
+            proxyRequest?.Headers?.Add(Constants.RouteTemplateHeader, templateRoute);
+            LogValueResolver.Register<RouteTemplateEnrichment>(() => templateRoute);
+
+            await next.Invoke();
+        }
+    }
+
+    public static class TemplateRouteFilterExtensions
+    {
+        public static void AddTemplateRouteMetrics(this MvcOptions options)
+        {
+            options.Filters.Add<TemplateRouteMetricsFilter>();
+        }
+    }
+}
