528e692ab82ca00609198163c7a88f1772f57a64
Mattias Liljenzin <mattias.liljenzin@gmail.com>
2019-09-24 10:43:55 +0200

    feat: remove mh_stack env

--- MhLabs.WebApi.Observability/Framework/Utilities/Settings.cs
+++ MhLabs.WebApi.Observability/Framework/Utilities/Settings.cs
@@ -1,16 +1,31 @@
 using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using MhLabs.Extensions;
 
 namespace MhLabs.WebApi.Observability.Framework.Logging
 {
     public class Settings
     {
         private const string UndefinedStack = "undefined_stack";
-        public static string Stack = Environment.GetEnvironmentVariable("mh_stack") ?? UndefinedStack;
+        public static string Stack = GetStackName();
         public static bool IsUndefinedStack => Stack == UndefinedStack;
 
         public static void Init(string stack)
         {
             Stack = stack ?? UndefinedStack;
+        }
+
+        public static string GetStackName()
+        {
+            const string LAMBDA_NAME = "AWS_LAMBDA_FUNCTION_NAME";
+            const string REGEX = @"(?<!^)(?=[A-Z])";
+
+            var env = Env.Get(LAMBDA_NAME);
+            if(env == null) return UndefinedStack;
+
+            return Regex.Split(env, REGEX).FirstOrDefault()?.TrimEnd('-') ?? UndefinedStack;
+            
         }
     }
 }
