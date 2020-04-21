ea8bc6c681546ba96bbb70114b6c265b7a6633aa
Marcus Casserfelt <marcus.casserfelt@gmail.com>
2020-04-21 10:33:07 +0200

    fix: added loggerutils to make testing of logs easier

---
+++ MhLabs.WebApi.Observability.Tests/LoggerUtilsTests.cs
@@ -0,0 +1,79 @@
+﻿using MhLabs.WebApi.Observability.Framework.Utilities;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace MhLabs.WebApi.Observability.Tests
+{
+    public class LoggerUtilsTests
+    {
+        [Fact]
+        public void TestLogInformation()
+        {
+            // arrange
+            var loggerMock = LoggerUtils.LoggerMock<MyHandler>();
+            var handler = new MyHandler(loggerMock.Object);
+            var expected = "Hello information";
+
+            // act
+            handler.LogInformation(expected);
+
+            // assert
+            loggerMock.VerifyLog(LogLevel.Information, expected);
+        }
+
+        [Fact]
+        public void TestLogWarning()
+        {
+            // arrange
+            var loggerMock = LoggerUtils.LoggerMock<MyHandler>();
+            var handler = new MyHandler(loggerMock.Object);
+            var expected = "Hello warning";
+
+            // act
+            handler.LogWarning(expected);
+
+            // assert
+            loggerMock.VerifyLog(LogLevel.Warning, expected);
+        }
+
+        [Fact]
+        public void TestLogError()
+        {
+            // arrange
+            var loggerMock = LoggerUtils.LoggerMock<MyHandler>();
+            var handler = new MyHandler(loggerMock.Object);
+            var expected = "Hello error";
+
+            // act
+            handler.LogError(expected);
+
+            // assert
+            loggerMock.VerifyLog(LogLevel.Error, expected);
+        }
+    }
+
+    public class MyHandler
+    {
+        private ILogger<MyHandler> _logger { get; }
+        public MyHandler(ILogger<MyHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public void LogInformation(string textToLog)
+        {
+            _logger.LogInformation(textToLog);
+        }
+
+        public void LogWarning(string textToLog)
+        {
+            _logger.LogWarning(textToLog);
+        }
+
+        public void LogError(string textToLog)
+        {
+            _logger.LogError(textToLog);
+        }
+
+    }
+}
---
+++ MhLabs.WebApi.Observability/Framework/Utilities/LoggerUtils.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+
+namespace MhLabs.WebApi.Observability.Framework.Utilities
+{
+    public static class LoggerUtils
+    {
+        /// <summary>
+        /// Creates a new instance of <pre>ILogger<T></pre>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static Mock<ILogger<T>> LoggerMock<T>() where T : class
+        {
+            return new Mock<ILogger<T>>();
+        }
+
+        /// <summary>
+        /// Verify that <see cref="string"/> has been logged one time
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="loggerMock"></param>
+        /// <param name="level"></param>
+        /// <param name="message"></param>
+        public static void VerifyLog<T>(this Mock<ILogger<T>> loggerMock, LogLevel level, string message)
+        {
+            loggerMock.VerifyLog(level, message, Times.Once());
+        }
+
+        /// <summary>
+        /// Verify that <see cref="string"/> has been logged x number of <see cref="Times"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="loggerMock"></param>
+        /// <param name="level"></param>
+        /// <param name="message"></param>
+        /// <param name="times"></param>
+        public static void VerifyLog<T>(this Mock<ILogger<T>> loggerMock, LogLevel level, string message, Times times)
+        {
+            loggerMock.Verify(l => l.Log<Object>(level, It.IsAny<EventId>(), It.Is<Object>(o => o.ToString() == message), null, It.IsAny<Func<Object, Exception, String>>()), times);
+        }
+    }
+}
